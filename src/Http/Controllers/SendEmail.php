563317614aa3b362c6b45eb188222076de126ef4
Hugo Hernández <hugo@doitcloud.consulting>
2025-09-14 01:16:45 -0600

    attaching files

--- src/Http/Controllers/SendEmail.php
+++ src/Http/Controllers/SendEmail.php
@@ -4,6 +4,7 @@ namespace Doitcloud\EmailService\Http\Controllers;
 
 use Doitcloud\EmailService\Http\Controllers\ConnectedAppMicrosoft;
 use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Storage;
 
 class SendEmail
 {
@@ -11,6 +12,7 @@ class SendEmail
     public $contentType;
     public $content;
     public $recipients = [];
+    public $relativePath;
 
     public function send()
     {
@@ -43,5 +45,62 @@ class SendEmail
                 ],
             ]
         ]);
+    }
+
+    public function sendWithAttachment()
+    {
+        // --- 1. Preparar el archivo adjunto ---
+        $filePath = Storage::disk('public')->path($this->relativePath); // Ruta a tu archivo en el servidor
+
+        // Validar que el archivo exista
+        if (!file_exists($filePath)) {
+            return response()->json(['error' => 'El archivo adjunto no existe.'], 404);
+        }
+
+        $fileName = basename($filePath); // Obtiene el nombre del archivo: "factura_septiembre.pdf"
+        $fileContent = file_get_contents($filePath); // Lee el contenido del archivo
+        $base64Content = base64_encode($fileContent); // Codifica el contenido a base64
+        $mimeType = mime_content_type($filePath); // Obtiene el tipo MIME: "application/pdf"
+
+
+        $connectedAppMicrosoft = new ConnectedAppMicrosoft();
+        $accessToken = $connectedAppMicrosoft->connectApp();
+
+
+        $client = new Client();
+
+        $response = $client->post(config('emailservice.send_email_url'), [
+            'headers' => [
+                'Authorization' => 'Bearer ' . $accessToken,
+                'Content-Type' => 'application/json',
+            ],
+            'json' => [
+                'message' => [
+                    'subject' =>$this->subject,
+                    'body' => [
+                        'contentType' => $this->contentType,
+                        'content' => $this->content,
+                    ],
+                    'toRecipients' => [
+                        [
+                            'emailAddress' => $this->recipients
+                            // [
+                            //     'address' => 'hugo@doitcloud.consulting',
+                            // ],
+                        ],
+                    ],
+                    'attachments' => [
+                        [
+                            '@odata.type' => '#microsoft.graph.fileAttachment',
+                            'name' => $fileName,
+                            'contentType' => $mimeType,
+                            'contentBytes' => $base64Content
+                        ]
+                    ]
+                ],
+            ]
+        ]);
+
+
     }
 }
